ab56784ab8bc1f1b720c0fac8f729f1678a58aba
Anthony <mecha06@mechatronics2-HP-xw4400-Workstation>
2016-10-19 21:51:12 -0700

    updated send_recieve and added helpers. Return parsing doesn't work quite right.

--- telescope_driver/spi_test.py
+++ telescope_driver/spi_test.py
@@ -15,13 +15,13 @@ import pyb
 from pyb import Pin
 from pyb import SPI
 
-spi = SPI(1, SPI.MASTER, polarity=1, phase=1, firstbit=SPI.MSB)
-chip_select = Pin(Pin.cpu.C5, Pin.OUT_PP)
+spi = SPI(2, SPI.MASTER, baudrate=4000000, polarity=1, phase=1, firstbit=SPI.MSB)
+chip_select = Pin(Pin.cpu.E7, Pin.OUT_PP)
 
 # Initialize chip select high
 chip_select.value(1)
 
-def send_recieve(send, recieve, chip_select):
+def send_recieve(send, recieve):
     """ A basic function utilizing micropython's send and recieve SPI commands with added chip
     select.
 
@@ -34,9 +34,40 @@ def send_recieve(send, recieve, chip_select):
         data (bytearray): The response from the SPI command
     """
 
+    data_bytes = [(send>> 8*(3-byte))&0xff for byte in range(0,4)]
+    recv_bytes = [0,0,0,0]
+    
+    # send data byte by byte
+    for byte in data_bytes:
+        __send_byte(byte, chip_select)
+        pyb.udelay(1)
+    # recieve data byte by byte
+    for byte in range(0,recieve):
+        recv_bytes[byte] = __read_byte(chip_select)
+        pyb.udelay(1)
+        #print (recv_bytes[byte])
+    
+    # convert recieved bytes into a single number
+    return_data = 0
+    for byte in range(0,recieve):
+        print("byte ", byte, "is ", recv_bytes[byte])
+        return_data += (list(reversed(recv_bytes))[byte])<<(8*byte)
+        print("return data is now ", return_data)
+    return return_data
+
+# sender helper
+def __send_byte (byte, chipsel):
     chip_select.value(0)
-    spi.send(send)
-    data = spi.recv(recieve)
+    pyb.udelay(1)
+    spi.send(byte)
+    pyb.udelay(1)
     chip_select.value(1)
 
-    return data
+# reciever helper
+def __read_byte (chipsel):
+    chip_select.value(0)
+    pyb.udelay(1)
+    data = spi.recv(1)
+    pyb.udelay(1)
+    chip_select.value(1)
+    return (data[0])
