afee99163657faeb2357f349238b75189434b631
Anthony <mecha06@mechatronics2-HP-xw4400-Workstation>
2016-10-21 22:56:33 -0700

    send_recieve() uses byte length values instead of bits now.

--- telescope_driver/spi_test.py
+++ telescope_driver/spi_test.py
@@ -14,7 +14,6 @@ Attributes:
 import pyb
 from pyb import Pin
 from pyb import SPI
-from math import ceil as math_ceil
 
 spi = SPI(2, SPI.MASTER, baudrate=4000000, polarity=1, phase=1, firstbit=SPI.MSB)
 chip_select = Pin(Pin.cpu.E7, Pin.OUT_PP)
@@ -27,16 +26,14 @@ def send_recieve(send, send_len, recieve_len):
             with added chip select.
     Args:
         send (int): The integer amount for the command you want to send
-        recieve (int): The number of bytes you want to read
-        chip_select (obj): Pin object for the chip select pin on the
-            microcontroller
+        send_len (int): The number of bytes being sent in the command
+        recieve_len (int): The number of bytes you want to read
     Returns:
-        data (bytearray): The response from the SPI command
+        data (int): The response from the SPI command
     """
     
-    byte_len = math_ceil(send_len/8)
     # breaks 'send' into bytes using a shift and mask.
-    data_bytes = [(send>> 8*(byte_len-byte-1))&0xff for byte in range(0,byte_len)]
+    data_bytes = [(send>> 8*(send_len-byte-1))&0xff for byte in range(0,send_len)]
     # prepare to fill this
     recv_bytes = []
     
@@ -45,14 +42,14 @@ def send_recieve(send, send_len, recieve_len):
         __send_byte(byte, chip_select)
         pyb.udelay(1)
     # recieve data byte by byte
-    for byte in range(0, math_ceil(recieve_len/8) ):
+    for byte in range(0, recieve_len):
         recv_bytes.append(__read_byte(chip_select))
         pyb.udelay(1)
         #print (recv_bytes[byte])
     
     # convert recieved bytes into a single number
     return_data = 0
-    for byte in range(0,math_ceil(recieve_len/8)):
+    for byte in range(0,recieve_len):
         # shift previous values over to make room
         return_data = return_data << 8
         # add the new byte in
